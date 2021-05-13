4a458cf178d473b9370450307b8c4800e0100031
DavidSharp <david.sharp@nationwide.co.uk>
2021-05-13 18:17:13 +0100

    working redux

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import {connect} from "react-redux"
+
 import './App.css';
 
-function App() {
+
+import {increaseCounter, decreaseCounter} from './redux/counter.actions'
+
+function App(props) {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Count is:</p>
+      <button onClick={() => props.increaseCounter()}>Increase counter</button>
+      <button onClick={() => props.decreaseCounter()}>Decrease counter</button>
     </div>
   );
 }
 
-export default App;
+//take state and put into props
+const mapStateToProps = (state) => {
+  return{
+    count: state.count
+  }
+}
+
+//make use of actions we declared in counter.actions.js
+const mapDispatchToProps = (dispatch) => {
+  return {
+    increaseCounter: () => dispatch(increaseCounter()),
+    decreaseCounter: () => dispatch(decreaseCounter())
+
+  }
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
